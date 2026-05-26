8b27a5031cf3c85962995a62d03213189004f33c
MamtaVenugopal <42015212+MamtaVenugopal@users.noreply.github.com>
2026-05-26 11:54:05 -0700

    [US-1779821102879] add dags/monthly_average_revenue_by_product_dag.py

---
+++ dags/monthly_average_revenue_by_product_dag.py
@@ -0,0 +1,69 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.emr import EmrCreateJobFlowOperator, EmrAddStepsOperator, EmrTerminateJobFlowOperator
+from datetime import datetime
+import os
+
+# Define the default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+    'retries': 1,
+}
+
+# Define the DAG
+with DAG('monthly_average_revenue_by_product_dag', default_args=default_args, schedule_interval='@monthly', catchup=False) as dag:
+    # Create EMR cluster
+    create_job_flow = EmrCreateJobFlowOperator(
+        task_id='create_job_flow',
+        job_flow_overrides={
+            'Name': 'Olist ETL',
+            'ReleaseLabel': 'emr-6.3.0',
+            'Instances': {
+                'InstanceGroups': [
+                    {
+                        'Name': 'Master',
+                        'InstanceRole': 'MASTER',
+                        'InstanceType': 'm5.xlarge',
+                        'InstanceCount': 1,
+                    },
+                    {
+                        'Name': 'Core',
+                        'InstanceRole': 'CORE',
+                        'InstanceType': 'm5.xlarge',
+                        'InstanceCount': 2,
+                    },
+                ],
+                'KeepJobFlowAliveWhenNoSteps': False,
+                'TerminationProtected': False,
+            },
+        },
+    )
+
+    # Add steps to the EMR cluster
+    add_steps = EmrAddStepsOperator(
+        task_id='add_steps',
+        job_flow_id=create_job_flow.output,
+        steps=[
+            {
+                'Name': 'Spark job for Monthly Average Revenue',
+                'ActionOnFailure': 'CONTINUE',
+                'HadoopJarStep': {
+                    'Jar': 'command-runner.jar',
+                    'Args': [
+                        'spark-submit',
+                        '--deploy-mode', 'cluster',
+                        's3://{}/src/jobs/monthly_average_revenue_by_product.py'.format(os.environ.get('S3_DATA_BUCKET'))
+                    ],
+                },
+            },
+        ],
+    )
+
+    # Terminate the EMR cluster
+    terminate_job_flow = EmrTerminateJobFlowOperator(
+        task_id='terminate_job_flow',
+        job_flow_id=create_job_flow.output,
+    )
+
+    # Set task dependencies
+    create_job_flow >> add_steps >> terminate_job_flow
